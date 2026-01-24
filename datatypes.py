507e96e93510a9abcca80407fd4e49006e72ccd2
yyuvan <yuvantoravi57@gmail.com>
2026-01-25 10:59:53 +1100

    Operators and Datatypes in python

---
+++ datatypes.py
@@ -0,0 +1,25 @@
+a=5
+b=10
+print(f"a={a} and b={b}")
+c=a
+a=b
+b=c
+print(f"a={a} and b={b}")
+
+
+is_raining=False
+print(is_raining)
+print(type(is_raining))
+print(int(is_raining))
+print(float(is_raining))
+
+is_student=True
+print(type(is_student))
+print(int(is_student))
+print(float(is_student))
+
+tempreture=19.7
+print(tempreture)
+print(type(tempreture))
+print(int(tempreture))
+print(bool(tempreture))
---
+++ operators.py
@@ -0,0 +1,22 @@
+# num1=int(input("Select a number: "))
+# num2=int(input("Select a number: "))
+
+# #Exponents in python
+# # print(num1**num2)
+
+# # print(num1/num2)
+# # print(num1//num2) #Rounds the number to the lowest integer
+
+# # % divides and give remainder
+# # print(16%5)
+
+# print(num1==num2)
+# print(num1!=num2)
+
+y=7
+x=5
+
+print((y!=x) == (x==y))
+
+print(y>x)
+print(y>=6)
