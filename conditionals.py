607892fee5df0c21636827e520e19bc3e8fa2867
yyuvan <yuvantoravi57@gmail.com>
2026-01-29 17:40:22 +1100

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,27 @@
+# age=int(input("What is your age? "))
+# if age>=18:
+#     print("You can vote")
+# else:
+#     print("You cannot vote")
+
+# print("You are at Dominoes and have $100, each burger is $5 ")
+# burger=int(input("How many burgers would you like? "))
+# discount=0
+# if burger<5:
+#     print("No discount")
+#     discount=1
+# elif burger<10:
+#     print("10% discount")
+#     discount=0.9
+# else:
+#     print("30% discount")
+#     discount=0.7
+# date=(input("Is it new year? Y for yes and N for no "))
+# if date=="Y":
+#     print("Additional 5%")
+#     discount=discount-0.05
+ 
+
+# total=5*burger*discount
+# total=100-total
+# print(f"You have ${total} left over, \t Have a nice day")
---
+++ date_time.py
@@ -0,0 +1,13 @@
+# import datetime, calendar
+# current_datetime=datetime.datetime.now()
+# print(current_datetime)
+# current_date=current_datetime.date()
+# print(current_date)
+# print(current_datetime.time())
+
+# print(calendar.calendar(2026))
+
+import math
+print(math.floor(8.7))
+print(math.ceil(8.7))
+print(math.pow(2,3))
