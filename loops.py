a45417700260058803d07883346a96417047dd19
yyuvan <yuvantoravi57@gmail.com>
2026-02-01 11:11:18 +1100

    stars

--- loops.py
+++ loops.py
@@ -16,14 +16,32 @@ Docstring for loops
 # print(f"1-50 sum = {x}")
 # print(sum(range(1,51)))
 
-num=int(input("Enter a number: "))
-root=int((num**0.5)+1)
-for i in range(2,root):
-    if num%i==0 :
-        print("Not a prime number")
-        break
-else:
-    print("This is a prime number")
+# num=int(input("Enter a number: "))
+# root=int((num**0.5)+1)
+# for i in range(2,root):
+#     if num%i==0 :
+#         print("Not a prime number")
+#         break
+# else:
+#     print("This is a prime number")
+
+'''
+*
+**
+***
+****
+*****
+'''
+
+for i in range(1,6):
+    for f in range(0,i):
+        print("*",end=" ")
+    print("\n")
+
+
+    # print("*"*i)
+ 
+ 
 
     
 
