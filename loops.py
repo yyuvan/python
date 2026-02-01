b33117c9e30c7d3ad39d8089339d63ff05987389
yyuvan <yuvantoravi57@gmail.com>
2026-02-01 11:01:55 +1100

    loops

---
+++ loops.py
@@ -0,0 +1,32 @@
+'''
+Docstring for loops
+2*1=2
+2*2=4
+2*3=6
+2*4=8
+2*5=10
+'''
+# for i in range(1,11):
+#     print(i)
+#     print(f"2*{i}={2*i}")
+
+# x=0
+# for i in range(1,51):
+#     x=x+i
+# print(f"1-50 sum = {x}")
+# print(sum(range(1,51)))
+
+num=int(input("Enter a number: "))
+root=int((num**0.5)+1)
+for i in range(2,root):
+    if num%i==0 :
+        print("Not a prime number")
+        break
+else:
+    print("This is a prime number")
+
+    
+
+
+
+
