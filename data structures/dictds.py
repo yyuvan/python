f7fe155bcafaa4375505a79c0756e972e1b742bf
yyuvan <yuvantoravi57@gmail.com>
2026-02-12 17:36:59 +1100

    ds

---
+++ data structures/dictds.py
@@ -0,0 +1,18 @@
+mydict=dict()
+print(mydict)
+mydict["Yuvan"]=15
+mydict["Rayan"]=15
+mydict["Adam"]=14
+mydict["Kaya"]=16
+
+print(mydict)
+
+mydict.update({"Che":14, "Mahi": 14})
+print(mydict)
+
+mydict["Yuvan"]=16
+print(mydict)
+
+mydict.clear()
+print(mydict)
+# print(mydict["Yuvan"])
---
+++ data structures/listds.py
@@ -0,0 +1,36 @@
+#Making a list
+fruits=["banana","apple","strawberry","lychee","pineapple"]
+# print(len(fruits))
+
+#Appending items
+fruits.append("pineapple")
+# print(fruits)
+
+#Inserting into lists
+fruits.insert(3, "Orange")
+# print(fruits)
+
+# print(fruits.count("pineapple"))
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+# print(fruits[0:3])
+# print(fruits[:3])
+# print(fruits[:])
+# print(fruits[::-1])
+# print(fruits[-1::-1])
+
+# print(fruits[4:6])
+
+# fruits.remove('pineapple')
+# print(fruits)
+
+# fruits.pop(1)
+# print(fruits)
+
+# fruits.reverse()
+# print(fruits)
+
+mynum=[123,32,59,38,65,928,9827]
+mynum.sort()
+print(mynum)
