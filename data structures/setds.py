8a5b9390d9d7efd3013f38f09fba2a34b97cfa2f
yyuvan <yuvantoravi57@gmail.com>
2026-02-15 10:47:20 +1100

    y

---
+++ data structures/setds.py
@@ -0,0 +1,32 @@
+myset={1,2,3,4,5,6,7,8,9,10,1,3,6,9,3}
+myset2={2,3,45,32,78,5,34,76,2,4,245,4,88,5,24,4,24,24}
+
+print(myset.union(myset2))
+print(myset.intersection(myset2))
+print(myset.difference(myset2))
+print(myset.isdisjoint(myset2))
+
+
+
+
+
+
+
+
+
+
+
+# print(myset)
+# myset.add(22)
+# print(myset)
+# myset.add(50)
+# print(myset)
+# myset.update([33,43,21,43,21,443,22,43,1,3,4,5,6,7,8,31,3])
+# print(myset)
+
+
+# myset.remove(2)
+# print(myset)
+
+# myset.discard(20202)
+# print(myset)
---
+++ data structures/tupleds.py
@@ -0,0 +1,14 @@
+mytuple=(3,4,5,7,5,2,6,5,2,34,6,7,87)
+print(mytuple[0])
+# mytuple[0]=4
+print(mytuple)
+print(mytuple.count(5))
+print(mytuple.index(5))
+
+
+
+
+
+
+
+
